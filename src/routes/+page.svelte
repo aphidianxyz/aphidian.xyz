1fecc4b3baa1a2f9bf04131b7353e2c7c85088a3
aphidianxyz <aphidiangithub@proton.me>
2025-07-20 21:57:08 -0400

    add styles for homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+    import '../app.css';
     const nav_emojis = ['📓','⚗️','🪲'];
-    const nav_entry = ['blog', 'projects', 'about'];
-    let dark_mode: Boolean = $state(false);
-</script>
+    const nav_entries = ['blog', 'projects', 'about'];
+    let dark_mode: Boolean = $state(false); </script>
 
 <svelte:head>
     <title>aphidian.xyz</title>
@@ -10,6 +10,7 @@
 
 <style>
 :global(body) {
+    background-color: #F5EDD1;
     margin: 0;
     padding: 0;
 }
@@ -18,42 +19,82 @@ main {
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 35vh;
-    margin-bottom: 0;
+    justify-content: center;
+    margin-top: 2.5vh;
+    min-height: 95vh;
 }
 
 section {
     display: flex;
+    align-items: center;
+    justify-items: center;
 }
 
-ul {
-    list-style: none;
+ul, footer {
+    font-family: AlegreyaSans;
+    font-weight: normal;
+    font-style: normal;
+}
+
+.sitetitle {
+    font-family: Alegreya;
+    font-weight: bold;
+    font-size: calc(9vw + 30px);
+    margin-left: 1vw; 
+    margin-top: 0;
+    margin-bottom: 1vh;
+}
+
+a:link.sitetitle, a:visited.sitetitle {
+    color: #1E2A18;
+    text-decoration: none;
+}
+
+a:link, a:visited {
+    color: #566A95;
+    text-decoration: none;
+}
+
+a:hover {
+    color: #91596D;
+    text-decoration: underline;
+}
+
+.logo {
+    border-color: black;
+    border-style: solid;
+    border-radius: 5px;
+    border-radius: 10px;
+    width: 12vw;
+    height: auto; 
+}
+
+footer {
+    width: 100%;
+    text-align: center;
+}
+
+nav {
+    font-size: calc(1vw + 20px);
 }
 </style>
 
 <main>
     <section>
-        <img
-            style:border-style="solid"
-            style:border-radius="10px"
-            src="." alt="aphidian logo"
-        >
-        <h1 
-            style:margin-left="10px"
-            style:font-size="500%"
-        >aphidian
-        </h1>
+        <!-- TODO: Replace placeholder img -->
+        <img class="logo" src="funny-dog-dog-on-a-balloon.gif" alt="aphidian logo">
+        <a class="sitetitle" href="/">aphidian</a>
     </section>
     <nav>
-        <ul>
-            {#each nav_entry as entry, emoji_index}
+        <ul style:list-style="none">
+            {#each nav_entries as entry, emoji_index}
                 <li>
                      <a href="/{entry}">{nav_emojis[emoji_index]} {entry}</a>
                 </li>
             {/each}
         </ul>
     </nav>
-    <footer>
-        <a href=".">aphidian.xyz</a>
-    </footer>
 </main>
+<footer>
+    <a href="/">aphidian.xyz</a>
+</footer>
