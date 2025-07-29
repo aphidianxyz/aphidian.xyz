14619e12978a90b1c471d697c3904b379575848b
aphidianxyz <aphidiangithub@proton.me>
2025-07-28 23:05:48 -0400

    add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     import type { LayoutProps } from './$types';
     let { data, children }: LayoutProps = $props();
 
-    import { darkMode } from './darkModeState.svelte'; 
+    import { darkMode } from './darkModeState.svelte';
     import Footer from '$lib/components/Footer.svelte';
 
     let palette = data.palette;
@@ -17,6 +17,15 @@
         margin: 0;
     }
 </style>
+
+<svelte:head>
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="aphidianxyz" />
+    <link rel="manifest" href="/site.webmanifest" />
+</svelte:head>
 
 <div 
     style:display="flex"
