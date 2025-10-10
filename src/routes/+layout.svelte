7b3ef3031e9c2017fa6df90069af2e58a1140cef
aphidianxyz <aphidiangithub@proton.me>
2025-10-10 01:33:56 -0400

    move favicons into their own directory

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,12 +30,12 @@
 </style>
 
 <svelte:head>
-    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" href="/icons/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/icons/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/icons/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/icons/favicon/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="aphidianxyz" />
-    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="manifest" href="/icons/favicon/site.webmanifest" />
 </svelte:head>
 
 <div 
