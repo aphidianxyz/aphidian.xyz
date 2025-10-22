26e0728e7eeb6f3ce5d1d908c5dcd36837d21164
aphidianxyz <aphidiangithub@proton.me>
2025-10-21 23:55:50 -0400

    first blog post

--- src/routes/blog/+layout.ts
+++ src/routes/blog/+layout.ts
@@ -13,28 +13,12 @@ export const load: LayoutLoad = () => {
     const entries: blogEntry[] = [
             // newer entries start here
             { 
-                slug: 'welcome-to-my-blog-tagless',
-                date: 'sep 24 2025',
-                title: 'Welcome to my tagless blog entry!',
-                tags: [],
-                contentFile: '/blogEntries/welcome-to-my-blog.html'
-            },
-            { 
-                slug: 'welcome-to-my-blog-the-sequel',
-                date: 'aug 07 2025',
-                title: 'Welcome to my blog! (the Sequel no one asked for)',
-                tags: ['meta',
-                    'second-post',
-                    'redundant-post'],
-                contentFile: '/blogEntries/welcome-to-my-blog.html'
-            },
-            { 
-                slug: 'welcome-to-my-blog',
-                date: 'aug 01 2025',
-                title: 'Welcome to my blog!',
+                slug: 'hello-world',
+                date: 'oct 12 2025',
+                title: 'hello world!',
                 tags: ['meta',
                     'first-post'],
-                contentFile: '/blogEntries/welcome-to-my-blog.html'
+                contentFile: '/blogEntries/hello-world.html'
             },
         ]
 
